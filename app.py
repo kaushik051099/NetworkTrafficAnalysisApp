d85b6af29fde3feef2d6a004181594479cbeeef3
Mimansha Kaushik <167587860+kaushik051099@users.noreply.github.com>
2025-01-09 23:28:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
 
 # Function to load and preprocess data
-def load_and_preprocess_data(file_path='https://raw.githubusercontent.com/kaushik051099/Projects/Time-Series-Analysis/Normal_trafficdata.csv'):
+def load_and_preprocess_data(file_path='https://raw.githubusercontent.com/kaushik051099/Projects//Normal_trafficdata.csv'):
     df = pd.read_csv(file_path)
     df['Time'] = pd.to_datetime(df['Time'], unit='s')  # Ensure correct conversion of time
     df.set_index('Time', inplace=True)
